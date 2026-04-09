439dcb8dad1637052c5da2d19683a0a2a0a5f197
Carl Beattie <contact.carlbeattie@gmail.com>
2026-04-09 15:04:25 +0100

    feat: mail service

---
+++ app/services/mail_service.ts
@@ -0,0 +1,34 @@
+import env from '#start/env'
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport(
+    env.get('NODE_ENV') === 'test' || env.get('NODE_ENV') === 'development'
+    ? ({ jsonTransport: true } as any)
+    : {
+        host: env.get('SMTP_HOST'),
+        port: env.get('SMTP_PORT'),
+        auth: {
+          user: env.get('SMTP_USER'),
+          pass: env.get('SMTP_PASS'),
+        },
+      }
+)
+
+export default class MailService {
+  static raw = transporter
+  private static from = env.get('SMTP_FROM')
+
+  static SendNewUserPassword(to: string, username: string, password: string) {
+    this.raw.sendMail({
+      from: this.from,
+      to,
+      subject: 'HarbourOS account created',
+      html: `
+        <h1>Welcome to HarbourOS</h1>
+        <p>Your username and password are below.</p>
+        <p>${username}</p>
+        <p>${password}</p>
+      `,
+    })
+  }
+}
