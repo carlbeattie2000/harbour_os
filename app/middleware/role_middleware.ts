a7d1d644fd6e84ca4ee385d54a453967ae87a82c
Carl Beattie <contact.carlbeattie@gmail.com>
2026-04-09 18:55:49 +0100

    feat: refactored rbac logic

--- app/middleware/role_middleware.ts
+++ app/middleware/role_middleware.ts
@@ -1,44 +1,35 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
-import { Forbidden } from '../errors/app_error.ts'
-import { DateTime } from 'luxon'
-import type Role from '#models/role'
-
-function roleIsValid(role: Role, roles: string[]) {
-  const isCorrectRole = roles.includes(role.slug)
-
-  const expiry = role.$extras.pivot_expires_at
-    ? DateTime.fromSQL(role.$extras.pivot_expires_at)
-    : null
-
-  const isNotExpired = !expiry || expiry > DateTime.now()
-
-  return isCorrectRole && isNotExpired
-}
+import { ForbiddenError } from '../errors/app_error.ts'
+import { RbacService } from '#services/rbac_service'
 
 export default class RoleMiddleware {
   async handle(
     ctx: HttpContext,
     next: NextFn,
-    requiredRole: string | string[],
-    strict: boolean = false
+    allowedRoles: string | string[],
+    disallowedRoles: string | string[] = [],
+    strictModeForAllowedRoles: boolean = false
   ) {
     const user = ctx.auth.getUserOrFail()
 
-    const rolesToVerify = Array.isArray(requiredRole) ? requiredRole : [requiredRole]
+    const allowedRolesToVerify = Array.isArray(allowedRoles) ? allowedRoles : [allowedRoles]
 
-    await user.load('roles')
+    const disallowedRolesToVerify = Array.isArray(disallowedRoles)
+      ? disallowedRoles
+      : [disallowedRoles]
 
-    const hasValidRole = strict
-      ? rolesToVerify.every((requiredSlug) => {
-          return user.roles.some((userRole) => roleIsValid(userRole, [requiredSlug]))
-        })
-      : user.roles.some((role) => {
-          return roleIsValid(role, rolesToVerify)
-        })
+    const usersRoles = await RbacService.getUserRoles(user)
 
-    if (!hasValidRole) {
-      throw new Forbidden()
+    if (
+      !RbacService.CanPerformAction(
+        usersRoles,
+        allowedRolesToVerify,
+        disallowedRolesToVerify,
+        strictModeForAllowedRoles
+      )
+    ) {
+      throw new ForbiddenError()
     }
 
     return await next()
---
+++ app/services/rbac_service.ts
@@ -0,0 +1,52 @@
+import type Role from '#models/role'
+import User from '#models/user'
+import { DateTime } from 'luxon'
+
+export class RbacService {
+  private static RoleHasExpired(role: Role) {
+    const expiry = role.$extras.pivot_expires_at
+      ? DateTime.fromSQL(role.$extras.pivot_expires_at)
+      : null
+
+    const isExpired = expiry && expiry <= DateTime.now()
+
+    return isExpired
+  }
+
+  static HasRole(userRoles: string[], roles: string[], strict: boolean): boolean {
+    return strict ? this.HasAllRoles(userRoles, roles) : roles.some((role) => userRoles.includes(role))
+  }
+
+  private static HasAllRoles(userRoles: string[], roles: string[]): boolean {
+    return roles.every((role) => userRoles.includes(role))
+  }
+
+  static UserRolesToSlugs(roles: Role[]): string[] {
+    const slugs = [];
+
+    for (const role of roles) {
+      if (this.RoleHasExpired(role)) continue;
+
+      slugs.push(role.slug);
+    }
+
+    return slugs;
+  }
+
+  static async getUserRoles(user: User): Promise<string[]> {
+    await user.load('roles', (query) => {
+      query.wherePivot('is_active', true);
+    });
+    await user.load('accountRoles', (query) => {
+      query.wherePivot('is_active', true);
+    });
+
+    return [...this.UserRolesToSlugs(user.roles), ...this.UserRolesToSlugs(user.accountRoles)];
+  }
+
+  static CanPerformAction(userRoles: string[], allowedRoles: string[], disallowedRoles: string[], allowedStrict: boolean) {
+    if (allowedRoles.length > 0 && !this.HasRole(userRoles, allowedRoles, allowedStrict)) return false;
+    if (disallowedRoles.length > 0 && this.HasRole(userRoles, disallowedRoles, false)) return false;
+    return true;
+  }
+}
