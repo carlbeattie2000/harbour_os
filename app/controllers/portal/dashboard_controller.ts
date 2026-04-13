fc1e3a7bcf54b4e05427c1f487b2dcc54532a40c
Carl Beattie <contact.carlbeattie@gmail.com>
2026-04-13 13:12:10 +0100

    feat: show accounts list on portal dashboard

--- app/controllers/portal/dashboard_controller.ts
+++ app/controllers/portal/dashboard_controller.ts
@@ -3,6 +3,9 @@ import type { HttpContext } from "@adonisjs/core/http";
 export default class DashboardController {
   async home({ view, auth }: HttpContext) {
     const user = auth.getUserOrFail();
+    await user.load('accounts', (query) => {
+      query.select('id').select('companyName')
+    });
     return view.render("pages/portal/home", { user });
   }
 }
