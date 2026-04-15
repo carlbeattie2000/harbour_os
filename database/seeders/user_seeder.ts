a733a1e0879a34cfe54671149defcb7973d5dd4d
Carl Beattie <contact.carlbeattie@gmail.com>
2026-04-15 18:57:20 +0100

    feat: user seeder creates a fake account for testing

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -1,36 +1,77 @@
+import {
+  ACCOUNT_DEFAULT_ROLE,
+  ACCOUNT_TYPE_ROLES,
+  type AccountType,
+} from '#constants/account_types'
+import Account from '#models/account'
+import AccountUser from '#models/account_user'
+import ContactDetail from '#models/contact_detail'
 import Role from '#models/role'
 import User from '#models/user'
+import UserAccountAssignedRole from '#models/user_account_assigned_role'
+import UserAccountRole from '#models/user_account_role'
 import UserAssignedRole from '#models/user_assigned_role'
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    const systemAdmin = await User.create({
-      firstName: 'system',
-      lastName: 'admin',
-      email: 'system_admin@shipping.com',
+    const internalAdmin = await User.create({
+      firstName: 'John',
+      lastName: 'Smith',
+      email: 'johnsmith@harbour-os.com',
       password: 'admin',
     })
 
     const adminRole = await Role.findByOrFail({ slug: 'admin' })
 
     await UserAssignedRole.create({
-      userId: systemAdmin.id,
+      userId: internalAdmin.id,
       roleId: adminRole.id,
     })
 
-    const portal = await User.create({
-      firstName: 'portal',
-      lastName: 'user',
-      email: 'portal@shipping.com',
+    const portalUser = await User.create({
+      firstName: 'Mike',
+      lastName: 'Smith',
+      email: 'mikesmith@shadowpeak.com',
       password: 'portal',
     })
 
     const portalRole = await Role.findByOrFail({ slug: 'portal' })
 
     await UserAssignedRole.create({
-      userId: portal.id,
+      userId: portalUser.id,
       roleId: portalRole.id,
+    })
+
+    const shippingLineAddress = await ContactDetail.create({
+      phone: '07444444444',
+      email: 'info@shadowpeak.com',
+      addressLine: '11 shadowpeak road',
+      country: 'United Kingdom',
+    })
+
+    const shippingLineAccount = await Account.create({
+      companyName: 'Shadow Peak Logistics',
+      type: 'shipping_line',
+      registrationNumber: '5454875454',
+      billingAddressId: shippingLineAddress.id,
+      status: 'active',
+    })
+
+    await AccountUser.create({
+      accountId: shippingLineAccount.id,
+      userId: portalUser.id,
+    })
+
+    const roles = ACCOUNT_TYPE_ROLES[shippingLineAccount.type as AccountType]
+    const createdRoles = await UserAccountRole.createMany(
+      roles.map((slug) => ({ accountId: shippingLineAccount.id, slug }))
+    )
+    const defaultRole = createdRoles.find((r) => r.slug === ACCOUNT_DEFAULT_ROLE)!
+
+    await UserAccountAssignedRole.create({
+      userId: portalUser.id,
+      roleId: defaultRole.id,
     })
   }
 }
